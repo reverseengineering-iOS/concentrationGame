a46e47553754011f0c5b374ef4e242540b412921
sai <saivishal.ios@gmail.com>
2018-03-28 18:38:54 -0400

    connected properties

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet var cardButton: [UIButton]!
+    var emojiChoices = ["👻","🎃","👻","🎃"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if  let cardNumber = cardButton.index(of: sender) {
+            flipCard(onEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("Choosen card was not there in the card Button")
+        }
     }
-
-
+    
+    func flipCard(onEmoji emoji: String, on button: UIButton){
+        if  button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+        
+    }
 }
 
